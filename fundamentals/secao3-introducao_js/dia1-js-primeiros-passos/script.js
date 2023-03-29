3104ce578d19b6c745b17bbc938d9758f2fc1369
Lucas <lcs.gabriel39@gmail.com>
2023-03-29 14:42:39 -0300

    novo dir

---
+++ fundamentals/secao3-introducao_js/dia1-js-primeiros-passos/script.js
@@ -0,0 +1,22 @@
+/*let custoProduto = 5
+let valorDeVenda = 10
+let lucro  = valorDeVenda - custoProduto 
+
+if (valorDeVenda > custoProduto) {
+  console.log(lucro*1000)
+}*/
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+console.log(names.push())
+console.log(names)
+
+/*for (let nomes of names) {
+  console.log(nomes)
+}*/
+
+/*let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);*/
