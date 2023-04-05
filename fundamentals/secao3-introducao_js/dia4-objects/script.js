906d1ca87494a8ba23656bf4ebaa8249b78d61de
lucazgbrl <lcs.gabriel39@gmail.com>
2023-04-04 23:01:03 -0300

    ex pedido cliente finalizado

--- fundamentals/secao3-introducao_js/dia4-objects/script.js
+++ fundamentals/secao3-introducao_js/dia4-objects/script.js
@@ -90,14 +90,16 @@ let order = {
 };
 
 function customerInfo(order) {
-  console.log(`Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, No ${order.address.number}, AP: ${order.address.apartment}.`
-  ) 
+  console.log(`Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, No ${order.address.number}, AP: ${order.address.apartment}.`) 
 }
 
 customerInfo(order);
 
 function orderModifier(order) {
-  
+  order.name = 'Luiz Silva'
+  order.payment.total = '50,00'
+
+  console.log(`Olá, ${order.name}, o valor total do seu pedido de ${Object.keys(order.order.pizza)} e ${order.order.drinks.coke.type} é R$${order.payment.total}`) 
 }
 
 orderModifier(order);
