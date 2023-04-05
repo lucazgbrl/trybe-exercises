aff8c54dd54d6db413dd3b3bf31a828f8bd0af82
lucazgbrl <lcs.gabriel39@gmail.com>
2023-04-04 22:49:02 -0300

    primeira parte ex pedido cliente

--- fundamentals/secao3-introducao_js/dia4-objects/script.js
+++ fundamentals/secao3-introducao_js/dia4-objects/script.js
@@ -30,7 +30,7 @@ for (value in info) {
   }
 }*/
 
-let leitor = {
+/*let leitor = {
   nome: 'Julia',
   sobrenome: 'Pessoa',
   idade: 21,
@@ -51,6 +51,53 @@ leitor.livrosFavoritos.push({
 
 console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}`)
 
-console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos.`)
+console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos.`)*/
 
+let order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
 
+function customerInfo(order) {
+  console.log(`Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, No ${order.address.number}, AP: ${order.address.apartment}.`
+  ) 
+}
+
+customerInfo(order);
+
+function orderModifier(order) {
+  
+}
+
+orderModifier(order);
