b67a09e479d401af520ea1d2211e37b98aeef1ba
lucazgbrl <lcs.gabriel39@gmail.com>
2023-04-05 12:04:56 -0300

    exercicios done

---
+++ fundamentals/secao3-introducao_js/dia5-es6-arrow-functions/script.js
@@ -0,0 +1,83 @@
+/*function imprimeIdade() {
+    for (let idade = 30; idade <= 40; idade += 1) {
+      console.log('Idade dentro do for:', idade)
+    }
+  }
+  imprimeIdade()*/
+
+  // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+  /*const pessoa = {
+    nome: 'Henri',
+    idade: 20
+  }
+  pessoa.nome = 'Luna',
+  pessoa.idade = 19 // Altere essa estrutura para corrigir o erro.
+  console.log('Nome:', pessoa.nome);
+  console.log('Idade:', pessoa.idade);*/
+
+  /*let favoriteFood = 'Lasanha';
+  favoriteFood = 'Hambúrguer';
+  console.log(favoriteFood);*/
+
+  /*const name = 'Adriana';
+  const lastName = 'Soares';
+  console.log(`Ola, ${name} ${lastName} !`);
+  function soma(a,b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let a = 3;
+  let b = 5;
+  console.log(`O resultado da soma de ${a} + ${b} e ${soma(a,b)} `);*/
+
+  /*const numeroAleatorio = () => {
+    return Math.random()
+  }
+  console.log(numeroAleatorio());*/
+
+  /*const hello = (nome) => {
+    return `Olá, ${nome}!`
+  }
+  let nome = 'Ivan';
+  console.log(hello(nome));*/
+
+  /*const nomeCompleto = (nome, sobrenome) => {
+    return `${nome} ${sobrenome}`
+  }
+  let nome = 'Ivan';
+  let sobrenome = 'Pires';
+  console.log(nomeCompleto(nome, sobrenome));
+
+  let speed = 90;
+  const speedCar = (speed) => {
+   return speed >= 120 ? `Você ultrapassou o limite de velocidade`: `Você está na velocidade permitida`;
+  };
+  console.log(speedCar(speed));*/
+
+  /*const turnOnOff = (car) => {
+    return car.motor == `ligado` ? car.motor = `desligado` : car.motor = `ligado`
+  }
+
+  const calculaAreaCirculo = (circulo) => {
+    console.log(`The circle calculated area is: ${Math.PI * (circulo.raio)**2}` ) 
+  }
+
+  let circulo = {
+    diametro: 15,
+    raio: 7.5 
+  }
+
+  calculaAreaCirculo(circulo)*/
+
+  /*const longestWord = (string) => {
+    let splitString =  string.split(' ')
+    let biggestWord = ' '
+    for(word in splitString){
+      splitString[word].length >= biggestWord.length ? biggestWord = splitString[word] : biggestWord = biggestWord
+    }
+    console.log(biggestWord) 
+  }
+
+  longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'*/
+
+  
