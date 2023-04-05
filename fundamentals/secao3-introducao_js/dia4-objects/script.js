6d78b16d251c2b1cfc127e85ddd47a36ad5afda4
lucazgbrl <lcs.gabriel39@gmail.com>
2023-04-04 22:24:15 -0300

    ex

--- fundamentals/secao3-introducao_js/dia4-objects/script.js
+++ fundamentals/secao3-introducao_js/dia4-objects/script.js
@@ -1,4 +1,4 @@
-let info = {
+/*let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
@@ -28,7 +28,29 @@ for (value in info) {
   } else {
     console.log(info[value] + ' e ' + info2[value])
   }
-}
+}*/
 
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+})
+
+console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}`)
+
+console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos.`)
 
 
